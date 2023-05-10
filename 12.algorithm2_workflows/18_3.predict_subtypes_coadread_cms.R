545d9392394ee25ad8ba2f6ef07a76bf8b919899
4to1stfloor <seokwon@ncc.re.kr>
2023-05-10 09:46:43 +0900

    modification_complete

--- 12.algorithm2_workflows/18_3.predict_subtypes_coadread_cms.R
+++ 12.algorithm2_workflows/18_3.predict_subtypes_coadread_cms.R
@@ -6,28 +6,32 @@ library(data.table)
 library(scater)
 library(scran)
 library(pheatmap)
+library(dplyr)
+library(stringr)
 setwd("~/nas/04.Results/subtypes/")
+
 # zscore normalization
+
 tcga.calc.zscore = function(sce, target.genes){
-  message("Calculating z-score with respect to all diploid cells. Version 2022.11.07")
+  message("Calculating z-score with respect to all diploid cells. Version 2023.05.03")
   common.genes = intersect(rownames(sce), target.genes)
   if (length(common.genes) == 0) {
     stop("None of the target genes found in sce. Please check your nomenclature")
-  } else if (all(common.genes %in% target.genes)) {
-    print("Check!")
+  } else if (length(common.genes) != length(target.genes)) {
+    message("Some of the genes from query does not exist in this cancer type. It will result in NAs")
+    message("Missing genes are: ", paste(setdiff(target.genes, common.genes), collapse = ", "))
   }
-  sce.sub = subset(sce, rownames(sce) %in% target.genes,)
+  sce.sub = subset(sce, rownames(sce) %in% common.genes,)
   #i am not checking assay names.
   count.mat = assay(sce.sub, 1)
   cnv.mat = assay(sce.sub, 3)
-  z.mat = matrix(data = NA, nrow = length(target.genes), ncol = ncol(count.mat))
+  z.mat = matrix(data = NA, nrow = nrow(count.mat), ncol = ncol(count.mat))
   colnames(z.mat) = colnames(count.mat)
-  rownames(z.mat) = target.genes
+  rownames(z.mat) = rownames(count.mat)
   for (i in 1:nrow(count.mat)) {
     idx.di = which(cnv.mat[i,] == 2)
     query.mean = mean(count.mat[i, idx.di], na.rm = T)
     query.sd = sd(count.mat[i, idx.di], na.rm = T)
-    idx.symb = which(rownames(z.mat) == rownames(count.mat)[i])
     z.mat[i,] = (count.mat[i,] - query.mean)/query.sd
   }
   return(z.mat)
@@ -38,7 +42,6 @@ ref_path = paste0(filepath, "99.reference/")
 sce_path = "/mnt/gluster_server/data/raw/TCGA_data/00.data/"
 
 cli = fread("~/nas/99.reference/all_clin_indexed.csv")
-setwd("~/nas/04.Results/compare_surv/")
 
 cms_genes = read_xlsx("~/nas/00.data/filtered_TCGA/34.TCGA-COADREAD/CMS_genes_merge.xlsx")
 cms = readRDS("~/nas/00.data/filtered_TCGA/34.TCGA-COADREAD/TCGA-COADREAD_CMS.rds")
@@ -54,8 +57,14 @@ CancerType = gsub('[.]','',gsub('\\d','', num_CancerType))
 
 sce = readRDS(paste0(sce_path,num_CancerType,"/", CancerType,".sce_raw_snv_cnv.rds"))
 mut = readRDS(paste0(sce_path,num_CancerType,"/", CancerType,"_maf.rds"))
+net = readRDS(paste0(main.path_tc, "/net_prop_total_587.rds"))
 
-sce_exp = tcga.calc.zscore(sce = sce, cms_genes$SYMBOL)
+cms_genes_sym = cms_genes$SYMBOL
+
+cms_genes_sym = cms_genes_sym[!cms_genes_sym %in% c("WARS","MLL2","MLL3", "MLL","HLAA", "HLAB", "AKD1", "MLLT4", "WHSC1" , "ERBB2IP", "PARK2", "WHSC1L1")]
+cms_genes_sym = c(cms_genes_sym , "WARS1", "KMT2D", "KMT2C", "KMT2A", "HLA-A", "HLA-B", "AK9", "AFDN", "NSD2", "ERBIN", "PRKN", "NSD3")
+
+sce_exp = tcga.calc.zscore(sce = sce, cms_genes_sym)
 sce_exp = as.data.frame(sce_exp)
 sce_exp[sce_exp > 5] <- 5
 sce_exp[sce_exp < -5] <- -5
@@ -87,7 +96,7 @@ names(CMS_colors) <- unique(annotation_df$CMS)
 
 
 png(filename = paste0(CancerType,"_exp_clusterT_CMS.png"),
-    width = 25, height = 25,  units = "cm" ,pointsize = 12,
+    width = 35, height = 35,  units = "cm" ,pointsize = 12,
     bg = "white", res = 1200, family = "")
 
 tmp = pheatmap::pheatmap(as.matrix(t(sce_exp_df[,-ncol(sce_exp_df)])),
@@ -95,38 +104,39 @@ tmp = pheatmap::pheatmap(as.matrix(t(sce_exp_df[,-ncol(sce_exp_df)])),
                          annotation_colors = list(CMS = CMS_colors),
                          cluster_cols = T)
 print(tmp)
-
+dev.off()
 
 png(filename = paste0(CancerType,"_exp_column_clusterF_CMS.png"),
-    width = 25, height = 25,  units = "cm" ,pointsize = 12,
+    width = 35, height = 35,  units = "cm" ,pointsize = 12,
     bg = "white", res = 1200, family = "")
 
-tmp = pheatmap::pheatmap(as.matrix(t(sce_exp_df[,-ncol(sce_exp_df)])),
-                         annotation_col = annotation_df,
-                         annotation_colors = list(CMS = CMS_colors),
-                         cluster_cols = F)
-print(tmp)
+tmp2 = pheatmap::pheatmap(as.matrix(t(sce_exp_df[,-ncol(sce_exp_df)])),
+                          annotation_col = annotation_df,
+                          annotation_colors = list(CMS = CMS_colors),
+                          cluster_cols = F)
+print(tmp2)
 
 dev.off()
 
 png(filename = paste0(CancerType,"_exp_cluster_complex_CMS.png"),
-    width = 25, height = 25,  units = "cm" ,pointsize = 12,
+    width = 35, height = 35,  units = "cm" ,pointsize = 12,
     bg = "white", res = 1200, family = "")
-tmp2 = ComplexHeatmap::pheatmap(as.matrix(t(sce_exp_df[,-ncol(sce_exp_df)])),
+tmp3 = ComplexHeatmap::pheatmap(as.matrix(t(sce_exp_df[,-ncol(sce_exp_df)])),
                                 column_split = annotation_df$CMS,
                                 annotation_col = annotation_df,
                                 annotation_colors = list(CMS = CMS_colors),
                                 cluster_cols = T)
 
-print(tmp2)
+print(tmp3)
+dev.off()
 
 ## mut
 mut_filtered <- subsetMaf(maf = mut, query = "Variant_Classification != 'Nonsense_Mutation'")
 
 mut_count = mutCountMatrix(mut_filtered)
-mut_count[cms_genes$SYMBOL,]
-mut_count_filtered = mut_count[which(rownames(mut_count) %in% cms_genes$SYMBOL),]
-nrow(mut_count_filtered)
+
+mut_count_filtered = mut_count[cms_genes_sym,]
+
 mut_count_filtered_tdf = as.data.frame(t(mut_count_filtered))
 
 dup_df = mut_count_filtered_tdf[which(duplicated(substr(rownames(mut_count_filtered_tdf),1,12))),]
@@ -140,7 +150,7 @@ for (dup_pat in rownames(dup_df)) {
   colnames(mean_df) <- colnames(mut_count_filtered_tdf[tmp[1],])
   rownames(mean_df) = substr(dup_pat,1,12)
   # remove two row and replace new mean row
-
+  
   mut_count_filtered_tdf <- subset(mut_count_filtered_tdf, rownames(mut_count_filtered_tdf) != tmp[1])
   mut_count_filtered_tdf <- subset(mut_count_filtered_tdf, rownames(mut_count_filtered_tdf) != tmp[2])
   
@@ -166,11 +176,10 @@ for (dup_pat in rownames(dup_cms_df)) {
     print(tmp_mut)
   }
 }
-if (all.equal(rownames(cms_filtered) , rownames(mut_count_filtered_tdf))) {
-  
-}
+
 
 rownames(cms_filtered) = substr(rownames(cms_filtered),1,12)
+
 common_pat = intersect(rownames(mut_count_filtered_tdf) , rownames(cms_filtered))
 mut_count_filtered_tdf_common = mut_count_filtered_tdf[common_pat,]
 cms_filtered_common = cms_filtered[common_pat,]
@@ -205,22 +214,139 @@ png(filename = paste0(CancerType,"_mut_clusterF_CMS_rowgene_col_pat.png"),
     width = 25, height = 25,  units = "cm" ,pointsize = 12,
     bg = "white", res = 1200, family = "")
 
-tmp = pheatmap::pheatmap(as.matrix(t(mut_count_filtered_tdf_common[,-ncol(mut_count_filtered_tdf_common)])),
-                         annotation_col = annotation_df,
-                         annotation_colors = list(CMS = CMS_colors),
-                         cluster_cols = F,show_rownames = F,show_colnames = F)
-print(tmp)
+tmp2 = pheatmap::pheatmap(as.matrix(t(mut_count_filtered_tdf_common[,-ncol(mut_count_filtered_tdf_common)])),
+                          annotation_col = annotation_df,
+                          annotation_colors = list(CMS = CMS_colors),
+                          cluster_cols = F,show_rownames = F,show_colnames = F)
+print(tmp2)
 
 dev.off()
 
 png(filename = paste0(CancerType,"_mut_cluster_complex_CMS.png"),
     width = 25, height = 25,  units = "cm" ,pointsize = 12,
     bg = "white", res = 1200, family = "")
-tmp2 = ComplexHeatmap::pheatmap(as.matrix(t(mut_count_filtered_tdf_common[,-ncol(mut_count_filtered_tdf_common)])),
+tmp3 = ComplexHeatmap::pheatmap(as.matrix(t(mut_count_filtered_tdf_common[,-ncol(mut_count_filtered_tdf_common)])),
                                 column_split = annotation_df$CMS,
                                 annotation_col = annotation_df,
                                 annotation_colors = list(CMS = CMS_colors),
                                 cluster_cols = T)
 
+print(tmp3)
+dev.off()
+
+## net
+net_cms = net[which(rownames(net) %in% cms_genes_sym),]
+
+net_cms_tdf = as.data.frame(t(net_cms))
+
+dup_net_df = net_cms_tdf[which(duplicated(substr(rownames(net_cms_tdf),1,12))),]
+
+for (dup_pat in rownames(dup_net_df)) {
+  tmp = grep(paste0(substr(dup_pat,1,12),"-*"), rownames(net_cms_tdf), value = TRUE)
+  
+  mean_df <- data.frame(lapply(seq_along(net_cms_tdf[tmp[1],]), function(i) {
+    round((net_cms_tdf[tmp[1],][[i]] + net_cms_tdf[tmp[2],][[i]]) / 2)
+  }))
+  colnames(mean_df) <- colnames(net_cms_tdf[tmp[1],])
+  rownames(mean_df) = substr(dup_pat,1,12)
+  # remove two row and replace new mean row
+  
+  net_cms_tdf <- subset(net_cms_tdf, rownames(net_cms_tdf) != tmp[1])
+  net_cms_tdf <- subset(net_cms_tdf, rownames(net_cms_tdf) != tmp[2])
+  
+  net_cms_tdf = rbind(net_cms_tdf, mean_df)
+  
+}
+
+rownames(net_cms_tdf) = substr(rownames(net_cms_tdf),1,12) 
+
+common_net_pat = intersect(rownames(net_cms_tdf) , rownames(cms_filtered))
+net_cms_tdf_common = net_cms_tdf[common_net_pat,]
+cms_net_filtered_common = cms_filtered[common_net_pat,]
+
+if (all.equal(rownames(net_cms_tdf_common), rownames(cms_net_filtered_common))) {
+  net_cms_tdf_common$CMS = cms_net_filtered_common$RF.details.RF.nearestCMS
+}
+
+net_cms_tdf_common = net_cms_tdf_common[order(net_cms_tdf_common$CMS),]
+
+annotation_df <- data.frame(CMS = net_cms_tdf_common$CMS)
+rownames(annotation_df) <- rownames(net_cms_tdf_common)
+
+# Create a named color vector for the unique values of vital_status
+CMS_colors <- c("CMS1" = "yellow", "CMS2" = "black", "CMS3" = "green", "CMS4" = "red")
+names(CMS_colors) <- unique(annotation_df$CMS)
+
+
+png(filename = paste0(CancerType,"_net_clusterT_CMS.png"),
+    width = 35, height = 35,  units = "cm" ,pointsize = 12,
+    bg = "white", res = 1200, family = "")
+
+tmp = pheatmap::pheatmap(as.matrix(t(net_cms_tdf_common[,-ncol(net_cms_tdf_common)])),
+                         annotation_col = annotation_df,
+                         annotation_colors = list(CMS = CMS_colors),
+                         cluster_cols = T)
+print(tmp)
+
+dev.off()
+
+png(filename = paste0(CancerType,"_net_clusterF_CMS.png"),
+    width = 35, height = 35,  units = "cm" ,pointsize = 12,
+    bg = "white", res = 1200, family = "")
+
+tmp2 = pheatmap::pheatmap(as.matrix(t(net_cms_tdf_common[,-ncol(net_cms_tdf_common)])),
+                          annotation_col = annotation_df,
+                          annotation_colors = list(CMS = CMS_colors),
+                          cluster_cols = F)
 print(tmp2)
+
+dev.off()
+
+png(filename = paste0(CancerType,"_net_cluster_complex_CMS.png"),
+    width = 35, height = 35,  units = "cm" ,pointsize = 12,
+    bg = "white", res = 1200, family = "")
+tmp3 = ComplexHeatmap::pheatmap(as.matrix(t(net_cms_tdf_common[,-ncol(net_cms_tdf_common)])),
+                                column_split = annotation_df$CMS,
+                                annotation_col = annotation_df,
+                                annotation_colors = list(CMS = CMS_colors),
+                                cluster_cols = T)
+
+print(tmp3)
+dev.off()
+
+#
+png(filename = paste0(CancerType,"_net_clusterT_CMS_rowgenes_colpat.png"),
+    width = 25, height = 25,  units = "cm" ,pointsize = 12,
+    bg = "white", res = 1200, family = "")
+
+tmp = pheatmap::pheatmap(as.matrix(t(net_cms_tdf_common[,-ncol(net_cms_tdf_common)])),
+                         annotation_col = annotation_df,
+                         annotation_colors = list(CMS = CMS_colors),
+                         cluster_cols = T,show_rownames = F,show_colnames = F)
+print(tmp)
+
+dev.off()
+
+png(filename = paste0(CancerType,"_net_clusterF_CMS_rowgenes_colpat.png"),
+    width = 35, height = 35,  units = "cm" ,pointsize = 12,
+    bg = "white", res = 1200, family = "")
+
+tmp2 = pheatmap::pheatmap(as.matrix(t(net_cms_tdf_common[,-ncol(net_cms_tdf_common)])),
+                          annotation_col = annotation_df,
+                          annotation_colors = list(CMS = CMS_colors),
+                          cluster_cols = F,show_rownames = F,show_colnames = F)
+print(tmp2)
+
+dev.off()
+
+png(filename = paste0(CancerType,"_net_cluster_complex_CMS_rowgenes_colpat.png"),
+    width = 35, height = 35,  units = "cm" ,pointsize = 12,
+    bg = "white", res = 1200, family = "")
+tmp3 = ComplexHeatmap::pheatmap(as.matrix(t(net_cms_tdf_common[,-ncol(net_cms_tdf_common)])),
+                                column_split = annotation_df$CMS,
+                                annotation_col = annotation_df,
+                                annotation_colors = list(CMS = CMS_colors),
+                                cluster_cols = T,show_rownames = F,show_colnames = F)
+
+print(tmp3)
 dev.off()
