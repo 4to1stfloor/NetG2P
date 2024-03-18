a0c47b2dd9f94eff171b368c1673254cc245c52b
4to1stfloor <seokwon@ncc.re.kr>
2024-03-18 15:11:42 +0900

    .

--- 12.algorithm2_workflows/25_19.depmap_sig_total_heatmap_figure4.R
+++ 12.algorithm2_workflows/25_19.depmap_sig_total_heatmap_figure4.R
@@ -94,6 +94,8 @@ pheatmap(total_rev,
          cluster_rows = F ,
          color = colorRampPalette(c("blue","white" ,"red"))(100))
 
+### short-long
+
 total_sig_matrix = data.frame()
 for (num_CancerType in Cancerlist) {
   
@@ -167,6 +169,107 @@ for (num_CancerType in Cancerlist) {
   
 }
 
+### delta
+num_CancerType = "10.TCGA-BLCA"
+total_sig_matrix = data.frame()
+total_sig_anno = data.frame()
+for (num_CancerType in Cancerlist) {
+  
+  main.path_tc = paste0(filepath, "00.data/filtered_TCGA/", num_CancerType)
+  CancerType = gsub('[.]','',gsub('\\d','', num_CancerType))
+  Cancername = gsub('TCGA-' , '', CancerType)
+  
+  # call input
+  
+  gc_TCGA = readRDS(paste0("~/nas/04.Results/short_long/", CancerType,"_critical_features_short_long.rds"))
+  gc_cellline_filt_df = readRDS(paste0("~/nas/04.Results/drug/depmap/gdsc/",CancerType, "_DM_sl_cluster.rds"))
+  
+  depmap_common_link_genes <- unique(link_genes_filtered_df[which(link_genes_filtered_df$Pathway %in% colnames(gc_cellline_filt_df %>% dplyr::select(-cluster))),]$Genes)
+  depmap_common_each_genes <- unique(single_genes[which(single_genes$Pathway %in% colnames(gc_cellline_filt_df %>% dplyr::select(-cluster))),]$Genes)
+  depmap_common_genes = unique(c(depmap_common_link_genes,depmap_common_each_genes))
+  # depmap_common_genes = unique(depmap_common_each_genes)
+  # 
+  depmap_score_critical_features = ge.tbl %>% 
+    filter(rownames(ge.tbl) %in% rownames(gc_cellline_filt_df)) %>%
+    dplyr::select(any_of(depmap_common_genes))
+  
+  common_depmap = gc_cellline_filt_df[rownames(depmap_score_critical_features),]
+  # at_depmap
+  if (all.equal(rownames(depmap_score_critical_features), rownames(common_depmap))) {
+    depmap_score_critical_features$cluster = common_depmap$cluster
+  }
+  
+  dep_score_cf_filt = depmap_score_critical_features %>% select(-names(which(colSums(is.na(depmap_score_critical_features)) != 0)))
+  # depmap_gene = "CFLAR"
+  
+  sig_genes = c()
+  for (depmap_gene in dep_score_cf_filt %>% select(-cluster) %>% colnames()) {
+    tmp_dep = dep_score_cf_filt %>% select(any_of(depmap_gene) , cluster)
+    
+    tmp_dep_short = tmp_dep %>% filter(cluster == "short") 
+    tmp_dep_long = tmp_dep %>% filter(cluster == "long")
+    
+    if (nrow(tmp_dep_short) < 2 | nrow(tmp_dep_long) < 2) {
+      next
+    }
+    anno_tmp = t.test(tmp_dep_short %>% 
+                        select(any_of(depmap_gene)) %>% 
+                        pull(), tmp_dep_long %>% 
+                        select(any_of(depmap_gene)) %>% 
+                        pull())$p.value
+    
+    if (anno_tmp < 0.03) {
+      sig_genes = c(sig_genes, depmap_gene)
+    } else {
+      next
+    }
+    
+  }
+  
+  if (length(sig_genes) == 0 )  {
+    next
+  }
+  dep_score_sig_cf_filt = dep_score_cf_filt[,c(sig_genes,"cluster")]
+  
+  long_dep_score_cf_filt = dep_score_sig_cf_filt %>% filter(cluster == "long")
+  short_dep_score_cf_filt = dep_score_sig_cf_filt %>% filter(cluster == "short")
+  
+  long_sum = long_dep_score_cf_filt %>% select(-cluster) %>% colMeans() %>% as.data.frame()
+  short_sum = short_dep_score_cf_filt %>% select(-cluster) %>% colMeans() %>% as.data.frame()
+  delta_sum = long_sum - short_sum
+  colnames(delta_sum) = c(paste0(Cancername , "_mean_delta"))
+  
+  delta_sum$direction = ""
+  if (nrow(delta_sum[which(long_sum < 0 & short_sum < 0 ),]) != 0) {
+    delta_sum[which(long_sum < 0 & short_sum < 0 ),]$direction = "both"
+  } 
+  
+  if (nrow(delta_sum[which(long_sum < 0 & short_sum > 0 ),]) != 0) {
+    delta_sum[which(long_sum < 0 & short_sum > 0 ),]$direction = "long"
+  } 
+  
+  if (nrow(delta_sum[which(long_sum > 0 & short_sum < 0 ),]) != 0) {
+    delta_sum[which(long_sum > 0 & short_sum < 0 ),]$direction = "short"
+  } 
+  
+  if (nrow(delta_sum[which(long_sum > 0 & short_sum > 0 ),]) != 0) {
+    delta_sum[which(long_sum > 0 & short_sum > 0 ),]$direction = "none"
+  }
+  
+  tmp_delta = as.data.frame(t(delta_sum %>% select(any_of(paste0(Cancername , "_mean_delta")))))
+  # tmp_anno = delta_sum %>% select(direction) %>% t() %>% as.data.frame()
+  tmp_anno = delta_sum %>% select(direction)
+  tmp_anno$genes = rownames(tmp_anno)
+  rownames(tmp_anno) = NULL
+  tmp_anno$cancertype = Cancername
+  tmp_anno = tmp_anno %>% select( cancertype, genes, direction)
+  # tmp_anno = delta_sum %>% select(direction) 
+  
+  total_sig_matrix = bind_rows(total_sig_matrix, tmp_delta)
+  total_sig_anno = rbind(total_sig_anno, tmp_anno)
+}
+
+
 total_sig_matrix
 
 total_sig_rev = -total_sig_matrix
@@ -202,9 +305,9 @@ total_sig_rev_back[total_sig_rev_back < 0] = (total_sig_rev_back[total_sig_rev_b
   (max(total_sig_rev_back[total_sig_rev_back < 0]) - min(total_sig_rev_back[total_sig_rev_back < 0])) *
   (max(total_sig_rev_back[total_sig_rev_back < 0]) - -1) + -1
 
-
 annotation_col = data.frame()
 
+# cancername = "CESC"
 for (cancername in unique(sapply(strsplit(rownames(total_sig_rev_back), "_"), "[", 1))) {
   
   nonzero_cols = colnames(total_sig_rev_back)[
@@ -213,19 +316,31 @@ for (cancername in unique(sapply(strsplit(rownames(total_sig_rev_back), "_"), "[
   ]
   
   tmp_annocol = data.frame(cancertype = cancername , genes = nonzero_cols)
+  tmp_sig_direct = total_sig_anno %>% filter(cancertype == cancername) %>% filter( genes %in% tmp_annocol$genes)
+  if (all.equal(tmp_annocol$genes , tmp_sig_direct$genes)) {
+    tmp_annocol$direction = tmp_sig_direct$direction
+  }
+ 
   annotation_col = rbind(annotation_col, tmp_annocol)
 }
+
+annotation_col
 
 other_df = data.frame(cancertype = "multiple_specific", genes = colnames(total_sig_rev_back)[!colnames(total_sig_rev_back) %in% annotation_col$genes])
 
 multi_genes = other_df$genes
 multi_only = total_sig_rev_back %>% 
   select(any_of(multi_genes))
-# mg = "CSF1"
+# mg = "COL9A3"
 other_sort_df = data.frame()
 for (mg in colnames(multi_only)) {
   tmp_m = multi_only %>% select(any_of(mg)) %>% filter(. !=0)
   tmp_other = data.frame(cancertype = unlist(strsplit(rownames(tmp_m)[which(tmp_m == max(tmp_m))], "_"))[1] , genes = mg)
+  
+  tmp_other = cbind(tmp_other , total_sig_anno %>% 
+                      filter(cancertype == tmp_other$cancertype) %>% 
+                      filter(genes == tmp_other$genes) %>% 
+                      select(direction))
   other_sort_df = rbind(other_sort_df,tmp_other) 
 }
 
@@ -234,16 +349,32 @@ rownames(annotation_col) <- annotation_col$genes
 annotation_col = annotation_col %>% select(-genes)
 annotation_col = annotation_col[colnames(total_sig_rev_back), , drop = FALSE]
 
-annotation_col = annotation_col %>% mutate(spe_cluster = case_when(rownames(.) %in% multi_genes ~ "multi_specific",
-                                                  .default = "cancer_specific"))
-
 annotation_row = data.frame(types = rownames(total_sig_rev_back))
 annotation_row  = annotation_row %>% 
-  mutate(cluster = sapply(strsplit(annotation_row$types, "_"), "[", 1)) %>%
-  mutate(sl = sapply(strsplit(annotation_row$types, "_"), "[", 2))
+  mutate(cluster = sapply(strsplit(annotation_row$types, "_"), "[", 1)) 
 rownames(annotation_row) = annotation_row$types
 
-annotation_row = annotation_row %>% select(-types) %>% select(sl, cluster)
+annotation_row = annotation_row %>% select(-types) %>% select(cluster)
+
+## re-order
+ordered_genes = c()
+for (cancername in unique(sapply(strsplit(rownames(total_sig_rev_back), "_"), "[", 1))) {
+  tmp_arrange = total_sig_rev_back %>% 
+    select(annotation_col %>% 
+             filter(cancertype == cancername) %>% 
+             rownames(.)) %>% 
+    filter(grepl(cancername, rownames(.)))
+  
+  tmp_ordered = tmp_arrange %>% gather(key = "gene", value = "value") %>% 
+    arrange(desc(value)) %>% pull(gene)
+  ordered_genes = c(ordered_genes, tmp_ordered)
+}
+
+if (sum(colnames(total_sig_rev_back) %in% ordered_genes) == ncol(total_sig_rev_back) ) {
+  total_sig_rev_back = total_sig_rev_back[,ordered_genes]
+} else {
+  print("error")
+}
 
 # c(
 #   "LGG" = "#00A087FF", # LGG
@@ -271,8 +402,10 @@ cancer_colors = c(
 )
 
 spe_colors = c(
-  "cancer_specific" = "#e62a89", # cancerspe
-  "multi_specific" = "#189d77" # muti
+  "both" = "#596CFF", # effective both direction
+  "long" = "#009E73", # effective both direction
+  "short" = "#FF3824", # effective both direction
+  "none" = "#FFDC24" # effective both direction
 )
 row_colors_cluster <- c(
   "LGG" = "#00A087FF", # LGG
@@ -286,9 +419,9 @@ row_colors_cluster <- c(
   "UCEC" = "#E64B35FF", # UCEC
   "LUAD" = "#F39B7FFF"  # LUAD
 )
-row_colors_sl <- c("short" = "red", "long" = "#009E73")
+# row_colors_sl <- c("short" = "red", "long" = "#009E73")
 
-ann_colors_sl = list(spe_cluster = spe_colors, cancertype = cancer_colors ,cluster = row_colors_cluster ,  sl =  row_colors_sl)
+ann_colors_sl = list(direction = spe_colors, cancertype = cancer_colors ,cluster = row_colors_cluster)
 
 ComplexHeatmap::pheatmap(total_sig_rev_back %>% as.matrix(),
                          cluster_rows = F,
@@ -297,7 +430,7 @@ ComplexHeatmap::pheatmap(total_sig_rev_back %>% as.matrix(),
                          column_split = factor(annotation_col$cancertype, levels = c(unique(annotation_col$cancertype))) ,
                          row_split = factor(annotation_row$cluster, levels = c(unique(annotation_row$cluster))),
                          annotation_colors = ann_colors_sl,
-                         annotation_col = annotation_col %>% select(spe_cluster),
+                         annotation_col = annotation_col %>% select(direction),
                          annotation_row = annotation_row,
                          legend = T,
                          annotation_legend = T,
@@ -309,9 +442,6 @@ ComplexHeatmap::pheatmap(total_sig_rev_back %>% as.matrix(),
                          color = c( colorRampPalette(c("#387eb8", "#F9FEFE", "#e21e26"))(100)),
                          scale = "none",
                          border_color = NA
-                         )
-
+                         ) 
 dev.off()
-#FCFDFE
-#F9FEFE
-#FDFDFE
+
