38d9e243139823d5cd80eb17bd3d86c1589abea7
4to1stfloor <seokwon@ncc.re.kr>
2024-08-29 15:23:25 +0900

    .

--- NetGPT/Critical Oncogenic Features module - split two group by importance.R
+++ NetGPT/Critical Oncogenic Features module - split two group by importance.R
@@ -11,151 +11,103 @@ library(dplyr)
 library(stringr)
 library(readxl)
 
-filepath = "/home/seokwon/nas/"
-ref_path = paste0(filepath, "99.reference/")
-Cancerlist = dir(paste0(filepath, "/00.data/filtered_TCGA/"))
-
-# for fic
-fig_path = "~/nas/04.Results/survival/"
-if(!dir.exists(fig_path)){
-  dir.create(fig_path)
-  print(paste0("Created folder: ", fig_path))
-} else {
-  print(paste0("Folder already exists: ", fig_path))
-}
-setwd(fig_path)
 set.seed(13524)
 
-# num_CancerType = "19.TCGA-LIHC"
-
 total_results_pval = data.frame()
-for (num_CancerType in Cancerlist) {
+
+CancerType = "Intered your cancer type"
+
+# call input
+annotate_best_features =  "your file : best_features.csv" # come from the output of extract important score.R
+duration_log_df = "your files" # you need the information of patients period data (suvial time). data/TCGA-XXX_dual_with_duration.rds
+
+best_features_df = duration_log_df[,annotate_best_features$variable]
+best_features_df$vitalstatus = duration_log_df$vitalstatus
+best_features_df$duration = duration_log_df$duration
+best_features_df = best_features_df[which(!is.na(best_features_df$duration)),]
+
+best_features_df$status = NA
+best_features_df$status[which(best_features_df$vitalstatus == "Dead")] = 1
+best_features_df$status[which(best_features_df$vitalstatus == "Alive")] = 0
+
+# results_pval
+result_surv_pval = data.frame(matrix(ncol = 1))
+colnames(result_surv_pval) = "pval"
+
+result_surv_pval$CancerType = NA
+result_surv_pval$num_of_features = NA
+result_surv_pval$diff_balance_ratio = NA
+
+for (last_num in 2:length(annotate_best_features$variable)) {
   
-  main.path_tc = paste0(filepath, "00.data/filtered_TCGA/", num_CancerType)
-  CancerType = gsub('[.]','',gsub('\\d','', num_CancerType))
+  out = pheatmap((best_features_df[,1:last_num] > -log(0.05))*1 , 
+                 cluster_cols = T,
+                 cluster_rows = T, 
+                 labels_cols = "",
+                 show_rownames = T,
+                 silent = T)
+  print(paste0(CancerType , "_start : ",last_num ))
+  print("----------------------------------------")
+  # print(table(cutree(out$tree_row, 2)))
   
-  # call input
-  annotate_best_features =  read.csv(paste0(filepath,"13.analysis/",CancerType,"_best_features.csv"))
-  duration_log_df = readRDS(paste0(main.path_tc, "/", CancerType,"_dual_add_duration_log.rds"))
+  tmp_pheat_cut = as.data.frame (cutree(out$tree_row, 2) , out[["tree_row"]][["labels"]])
+  colnames(tmp_pheat_cut) = "cluster"
   
-  # results_pval
-  result_surv_pval = data.frame(matrix(ncol = 1))
-  colnames(result_surv_pval) = "pval"
+  best_features_df$cluster = tmp_pheat_cut$cluster
   
-  best_features_df = duration_log_df[,annotate_best_features$variable]
-  best_features_df$vitalstatus = duration_log_df$vitalstatus
-  best_features_df$duration = duration_log_df$duration
-  best_features_df = best_features_df[which(!is.na(best_features_df$duration)),]
-  
-  best_features_df$status = NA
-  best_features_df$status[which(best_features_df$vitalstatus == "Dead")] = 1
-  best_features_df$status[which(best_features_df$vitalstatus == "Alive")] = 0
-  
-  result_surv_pval$CancerType = NA
-  result_surv_pval$num_of_features = NA
-  result_surv_pval$diff_balance_ratio = NA
-  
-  for (last_num in 2:length(annotate_best_features$variable)) {
-    
-    out = pheatmap((best_features_df[,1:last_num] > -log(0.05))*1 , 
-                   cluster_cols = T,
-                   cluster_rows = T, 
-                   labels_cols = "",
-                   show_rownames = T,
-                   silent = T)
-    print(paste0(CancerType , "_start : ",last_num ))
-    print("----------------------------------------")
-    # print(table(cutree(out$tree_row, 2)))
-    
-    tmp_pheat_cut = as.data.frame (cutree(out$tree_row, 2) , out[["tree_row"]][["labels"]])
-    colnames(tmp_pheat_cut) = "cluster"
-    
-    best_features_df$cluster = tmp_pheat_cut$cluster
-    
-    if (mean(best_features_df[which(best_features_df$cluster == 1),]$duration) <
-        mean(best_features_df[which(best_features_df$cluster == 2),]$duration)) {
-      best_features_df[which(best_features_df$cluster == 1),]$cluster = 3
-      best_features_df[which(best_features_df$cluster == 2),]$cluster = 1
-      best_features_df[which(best_features_df$cluster == 3),]$cluster = 2
-    } else {
-      best_features_df = best_features_df
-    }
-    
-    fit = survfit(Surv(duration, status) ~ cluster, data = best_features_df)
-    
-    png(filename = paste0(CancerType, "_",last_num,"_survival_plot_adjust.png"),
-        width = 35, height = 35,  units = "cm" ,pointsize = 12,
-        bg = "white", res = 1200, family = "")
-    
-    tmp_surv = ggsurvplot(
-      fit,    # survfit object with calculated statistics.
-      data = best_features_df,
-      pval = TRUE,             # show p-value of log-rank test.
-      conf.int = TRUE,         # show confidence intervals for 
-      ggtheme = RTCGA::theme_RTCGA(),
-      risk.table = TRUE,
-      xlab = "days",
-      surv.median.line = "hv"
-    )
-    
-    print(tmp_surv)
-    
-    dev.off()
-    
-    result_surv_pval[last_num,"num_of_features"] = last_num
-    result_surv_pval[last_num,"pval"] = surv_pvalue(fit)$pval
-    result_surv_pval[last_num,"CancerType"] = CancerType
-    # result_surv_pval[last_num,"balance_ratio_1"] = round(fit$n[1] / (fit$n[1] + fit$n[2]), 2)
-    # result_surv_pval[last_num,"balance_ratio_2"] = round(fit$n[2] / (fit$n[1] + fit$n[2]), 2)
-    
-    if (round(fit$n[1] / (fit$n[1] + fit$n[2]), 2) > round(fit$n[2] / (fit$n[1] + fit$n[2]), 2) ) {
-      result_surv_pval[last_num,"diff_balance_ratio"] = round(fit$n[1] / (fit$n[1] + fit$n[2]), 2) - round(fit$n[2] / (fit$n[1] + fit$n[2]), 2)
-    } else {
-      result_surv_pval[last_num,"diff_balance_ratio"] = round(fit$n[2] / (fit$n[1] + fit$n[2]), 2) - round(fit$n[1] / (fit$n[1] + fit$n[2]), 2) 
-    }
-    
-    if (round(fit$n[1] / (fit$n[1] + fit$n[2]), 2) <= 0.7 && round(fit$n[1] / (fit$n[1] + fit$n[2]), 2) >= 0.3) {
-      result_surv_pval[last_num,"bias"] = "balanced"
-    } else {
-      result_surv_pval[last_num,"bias"] = "not balanced"
-    }
-    
-    remove(tmp_pheat_cut,fit,plot_roc)
-  }
-  
-  result_surv_pval = na.omit(result_surv_pval)
-  result_surv_pval_spe = result_surv_pval[which(result_surv_pval$pval < 0.05),]
-  
-  if (length(result_surv_pval_spe$pval) == 0) {
-    
-    tmp_diff = result_surv_pval[which(result_surv_pval$diff_balance_ratio < 0.4),]
-    
-    if (nrow(tmp_diff[which(tmp_diff$pval == min(tmp_diff$pval)),]) > 1) {
-      tmp_diff_pval =tmp_diff[which(tmp_diff$pval == min(tmp_diff$pval)),]
-      total_results_pval = rbind(total_results_pval, tmp_diff_pval[which(tmp_diff_pval$num_of_features == max(tmp_diff_pval$num_of_features)),])
-    } else {
-      total_results_pval = rbind(total_results_pval, tmp_diff[which(tmp_diff$pval == min(tmp_diff$pval)),])
-    }
-    
-  } else if (length(unique(result_surv_pval_spe$bias)) == 1 && unique(result_surv_pval_spe$bias == "not balanced") ) {
-    # total_results_pval = rbind(total_results_pval, result_surv_pval_spe[which(result_surv_pval_spe$pval == min(result_surv_pval_spe$pval)),])
-    
-    if (nrow(result_surv_pval_spe[which(result_surv_pval_spe$diff_balance_ratio == min(result_surv_pval_spe$diff_balance_ratio)),]) > 1) {
-      tmp_diff = result_surv_pval_spe[which(result_surv_pval_spe$diff_balance_ratio == min(result_surv_pval_spe$diff_balance_ratio)),]
-      total_results_pval = rbind(total_results_pval, tmp_diff[which(tmp_diff$pval == min(tmp_diff$pval)),])
-    } else {
-      total_results_pval = rbind(total_results_pval, result_surv_pval_spe[which(result_surv_pval_spe$diff_balance_ratio == min(result_surv_pval_spe$diff_balance_ratio)),])
-    }
-    
+  if (mean(best_features_df[which(best_features_df$cluster == 1),]$duration) <
+      mean(best_features_df[which(best_features_df$cluster == 2),]$duration)) {
+    best_features_df[which(best_features_df$cluster == 1),]$cluster = 3
+    best_features_df[which(best_features_df$cluster == 2),]$cluster = 1
+    best_features_df[which(best_features_df$cluster == 3),]$cluster = 2
   } else {
-    tmp_bias_pval = result_surv_pval_spe[which(result_surv_pval_spe$bias == "balanced"),]
-    total_results_pval = rbind(total_results_pval, tmp_bias_pval[which(tmp_bias_pval$pval == min(tmp_bias_pval$pval)),])
+    best_features_df = best_features_df
   }
   
-  write.csv(result_surv_pval, paste0(CancerType, "_rawdata_survpval.csv"))
-  remove(best_features_df,annotate_best_features,duration_log_df,result_surv_pval)
+  fit = survfit(Surv(duration, status) ~ cluster, data = best_features_df)
+  
+  # ### if you wana print the plot, plz proceed below annotated code
+  # png(filename = paste0(CancerType, "_",last_num,"_survival_plot_adjust.png"),
+  #     width = 35, height = 35,  units = "cm" ,pointsize = 12,
+  #     bg = "white", res = 1200, family = "")
+  # 
+  # tmp_surv = ggsurvplot(
+  #   fit,    # survfit object with calculated statistics.
+  #   data = best_features_df,
+  #   pval = TRUE,             # show p-value of log-rank test.
+  #   conf.int = TRUE,         # show confidence intervals for 
+  #   ggtheme = RTCGA::theme_RTCGA(),
+  #   risk.table = TRUE,
+  #   xlab = "days",
+  #   surv.median.line = "hv"
+  # )
+  # 
+  # print(tmp_surv)
+  # 
+  # dev.off()
+  # ###
+  
+  result_surv_pval[last_num,"num_of_features"] = last_num
+  result_surv_pval[last_num,"pval"] = surv_pvalue(fit)$pval
+  result_surv_pval[last_num,"CancerType"] = CancerType
+  
+  if (round(fit$n[1] / (fit$n[1] + fit$n[2]), 2) > round(fit$n[2] / (fit$n[1] + fit$n[2]), 2) ) {
+    result_surv_pval[last_num,"diff_balance_ratio"] = round(fit$n[1] / (fit$n[1] + fit$n[2]), 2) - round(fit$n[2] / (fit$n[1] + fit$n[2]), 2)
+  } else {
+    result_surv_pval[last_num,"diff_balance_ratio"] = round(fit$n[2] / (fit$n[1] + fit$n[2]), 2) - round(fit$n[1] / (fit$n[1] + fit$n[2]), 2) 
+  }
+  
+  if (round(fit$n[1] / (fit$n[1] + fit$n[2]), 2) <= 0.7 && round(fit$n[1] / (fit$n[1] + fit$n[2]), 2) >= 0.3) {
+    result_surv_pval[last_num,"bias"] = "balanced"
+  } else {
+    result_surv_pval[last_num,"bias"] = "not balanced"
+  }
+  
+  remove(tmp_pheat_cut,fit,plot_roc)
 }
-library(openxlsx)
-write.xlsx(total_results_pval, paste0("Total_results_survpval3.xlsx"))
+
+# save the result
+write.csv(result_surv_pval, paste0(CancerType, "_rawdata_survpval.csv"))
+
 
 
